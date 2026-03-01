e830aa1694dbd5965197944e4a40d1b49e365517
ctannraj <161249622+ctannraj@users.noreply.github.com>
2026-02-28 16:43:30 -0800

    renamed file

--- src/main/java/com/mnc/instagram/InstagramLiteDockerApplication.java
+++ src/main/java/com/mnc/instagram/InstagramLiteDockerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InstagramLiteInMemoryApplication {
+public class InstagramLiteDockerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InstagramLiteInMemoryApplication.class, args);
+        SpringApplication.run(InstagramLiteDockerApplication.class, args);
     }
 }
